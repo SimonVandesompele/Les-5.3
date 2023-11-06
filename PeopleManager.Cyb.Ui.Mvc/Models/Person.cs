006d532a3afea292be4aa713e3e42f8257899048
SimonVandesompele <115065783+SimonVandesompele@users.noreply.github.com>
2023-11-06 10:02:18 +0100

    Initial commit

---
+++ PeopleManager.Cyb.Ui.Mvc/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PeopleManager.Cyb.Ui.Mvc.Models
+{
+    [Table(nameof(Person))]
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Required]
+        public required string LastName { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+    }
+}
